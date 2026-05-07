1820c25f2ee6df33574927f6310def7ce83e4a62
Jack <71994438+Acegameboy@users.noreply.github.com>
2026-05-07 16:31:43 -0700

    Fixed missing content in TransformComponent

--- Engine/VEngine/Inc/TransformComponent.h
+++ Engine/VEngine/Inc/TransformComponent.h
@@ -7,10 +7,11 @@ namespace VEngine
 	class TransformComponent final : public Component, public Graphics::Transform
 	{
 	public:
-		SET_TYPE_ID(ComponentId::Transfrom);
+		SET_TYPE_ID(ComponentId::Transform);
 
 		void DebugUI() override;
-		
 
+		void Deserialize(const rapidjson::Value& value) override;
+		void Serialize();
 	};
 }
--- Engine/VEngine/Src/TransformComponent.cpp
+++ Engine/VEngine/Src/TransformComponent.cpp
@@ -1,5 +1,6 @@
 #include "Precompiled.h"
 #include "TransformComponent.h"
+#include "SaveUtil.h"
 
 using namespace VEngine;
 using namespace VEngine::Graphics;
@@ -11,4 +12,11 @@ void VEngine::TransformComponent::DebugUI()
 	ImGui::DragFloat3("Scale", &scale.x, 0.1f);
 
 	SimpleDraw::AddTransform(GetMatrix4());
+}
+
+void VEngine::TransformComponent::Deserialize(const rapidjson::Value& value)
+{
+	SaveUtil::ReadVector3("Position", position, value);
+	SaveUtil::ReadQuaternion("Rotation", rotation, value);
+	SaveUtil::ReadVector3("Scale", scale, value);
 }
